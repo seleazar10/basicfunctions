33f93f876157e6104025fc6171bbe7b9a01fca92
Stama Eleazar <eleoatam10@gmail.com>
2020-03-20 10:00:00 -0500

    css and script added

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+var laps = []
+for(var i=0; i<=10; i++){
+
+    function calculateAge(year){
+
+        var myAge = year - 2000;
+        console.log(myAge)
+        laps.push(myAge)
+
+
+    }
+
+    console.log(laps)
+
+}
+
+calculateAge(2020)
