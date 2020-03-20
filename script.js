dbf99cfa76ba72cc7fd08c4a4d5f6ebf4e1a1d5c
Stama Eleazar <eleoatam10@gmail.com>
2020-03-20 10:27:04 -0500

    added function to check age

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 
 
-var laps = []
-for(var i=0; i<=10; i++){
 
-    function calculateAge(year){
+/////Loop 
 
-        var myAge = year - 2000;
-        console.log(myAge)
-        laps.push(myAge)
+// var laps = []
+// for(var i=0; i<=10; i++){
 
 
-    }
+//     laps.push(i)
 
-    console.log(laps)
+//     console.log(laps)
+
+// }
+
+// calculateAge(2020)
+
+
+
+/////-----------Check Age App
+
+function calculateAge(birthYear) {
+
+    var custoAge = 2020 - birthYear;
+    console.log(custoAge + " years old")
 
 }
 
-calculateAge(2020)
+
+function underageCheck(year, custoName){
+     
+    
+    var age = calculateAge(year)
+    var minAge = 21;
+
+    if(age >= minAge){
+        console.log(custoName + ' is good to go!')
+    }else{
+        console.log(custoName + ' is underaged.')
+    }
+}
+
+
+// calculateAge(1990, 'John')
+
+underageCheck(1990, 'John')
